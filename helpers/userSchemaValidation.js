f21023173e8fd2c39186f9a9d6013a6180beb53c
CamiWallace <59678253+CamiWallace@users.noreply.github.com>
2022-05-25 19:48:41 -0600

    fixed joi validation
    Signed-off-by: CamiWallace <59678253+CamiWallace@users.noreply.github.com>

--- helpers/userSchemaValidation.js
+++ helpers/userSchemaValidation.js
@@ -2,14 +2,14 @@ const Joi = require('@hapi/joi')
 //schema for post endpoint
 const postSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.email().required(),
+    email: Joi.string().email().required(),
     thirdPartyLink: Joi.string().uri()
 })
 
 //schema for post endpoint
 const putSchema = Joi.object({
     name: Joi.string(),
-    email: Joi.email(),
+    email: Joi.string().email(),
     thirdPartyLink: Joi.string().uri()
 })
 
